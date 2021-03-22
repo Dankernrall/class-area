64256d6f39bf9393bf5519f58f208bd8d75741e8
Никита <grand.marchello@gmail.com>
2021-03-22 18:21:13 +0400

    Решение задания 2

--- src/main/java/Area.java
+++ src/main/java/Area.java
@@ -1,26 +1,28 @@
 public class Area {
     static double getArea(double radius) {
-        if (radius <1){return 0;}
-        else{
-        double first = Math.PI * Math.pow(radius, 2);
-        double result = Math.floor(first*100)/100;
-        return first;}
+        if (radius < 1) {
+            return 0;
+        } else {
+            return Math.PI * Math.pow(radius, 2);
+        }
     }
 
     static double getArea(int width, int length) {
-        if (width < 1 | length < 1){return 0;}
-        else{
-        double first = width * length;
-        double result = Math.floor(first*100)/100;
-        return result;}
+        if (width < 1 | length < 1) {
+            return 0;
+        } else {
+            double first = width * length;
+            return Math.floor(first * 100) / 100;
+        }
     }
 
     static double getArea(double radius, double height) {
-            if (radius <1 | height < 1){return 0;}
-            else{
-        double first = 2 * Math.PI * radius * (radius + height);
-        double result = Math.floor(first*100)/100;
-        return result;}
+        if (radius < 1 | height < 1) {
+            return 0;
+        } else {
+            double first = 2 * Math.PI * radius * (radius + height);
+            return Math.floor(first * 100) / 100;
+        }
     }
 
     public static void main(String[] args) {
